7f8ab3889a6f844e814be823b796e25c0b928108
kuldeep <kuldeep@kuldeep-PC>
2017-04-14 00:55:16 +0530

    added country related code

--- PSK/src/psk/com/WelcomePage.java
+++ PSK/src/psk/com/WelcomePage.java
@@ -1,15 +1,21 @@
 package psk.com;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WelcomePage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Hello");
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\kuldeep\\Documents\\GicDriver\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://pstakecare.com/");
+		WebDriverWait wait=new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='city-toggle-slide']/section/section[2]/div[2]/button"))).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='site-header']/section/section[1]/div[4]/div"))).click();
 		
 
 	}
