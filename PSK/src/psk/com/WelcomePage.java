94c88c2f74dae1db022a086c27ba728768d1e2de
kuldeepQA <kuldeepjadon39@gmail.com>
2017-04-14 00:57:16 +0530

    Update WelcomePage.java
    remove extra code

--- PSK/src/psk/com/WelcomePage.java
+++ PSK/src/psk/com/WelcomePage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class WelcomePage {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Hello");
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\kuldeep\\Documents\\GicDriver\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://pstakecare.com/");
