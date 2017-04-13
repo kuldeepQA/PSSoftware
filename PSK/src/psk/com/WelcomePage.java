64895b843844e5ce53db8af918ac41ed6405242d
kuldeep <kuldeep@kuldeep-PC>
2017-04-13 23:27:40 +0530

    i have adde url invoke code

--- PSK/src/psk/com/WelcomePage.java
+++ PSK/src/psk/com/WelcomePage.java
@@ -1,10 +1,16 @@
 package psk.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WelcomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\kuldeep\\Documents\\GicDriver\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://pstakecare.com/");
+		
 
 	}
 
